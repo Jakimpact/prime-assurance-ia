6ff4ee53f63a05b1479275636bdb214de2ae3525
SimonRqrt <simon.riquart@icloud.com>
2024-01-11 16:13:05 +0100

    Mise à jour streamlit

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ if menu == "Accueil":
     st.title("Affichage des graphiques de données")
     graph = st.selectbox("Types de graphiques :",["Diagramme de points","Diagramme de barres","Histogramme"])
     if graph == "Diagramme de points":
-        value = st.slider("Filtrer les données selon le nombre d'enfants",0,5)
-        data = data.loc[data["children"]>=value]
+        value = st.slider("Filtrer les données selon l'age",18,65)
+        data = data.loc[data["age"]<=value]
         fig,ax = plt.subplots(figsize=(10,5))
         sns.scatterplot(data=data , x="bmi", y="charges",hue="smoker")
         st.pyplot(fig)
@@ -48,3 +48,19 @@ if menu == "Accueil":
         fig,ax = plt.subplots(figsize=(5,3))
         sns.displot(data,x="smoker",y="charges")
         st.pyplot(fig)
+
+if menu == "Prédiction d'assurance":
+    st.header("Prédiction des charges d'un nouvel assuré")
+    with st.form("Nouvel assuré"):
+        st.write("Nouvel assuré")
+        value_1 = st.text_input("Age:")
+        value_2 = st.text_input("IMC:")
+        value_2 = st.selectbox("Sexe:",["Homme","Femme"])
+        value_2 = st.selectbox("Nombre d'enfants:",["0","1","2","3","4","5","6"])
+        value_2 = st.selectbox("Fumeur:",["Oui","Non"])
+        value_2 = st.selectbox("Région:",["Northwest","Southwest","Northeast","Southeast"])
+
+        submitted = st.form_submit_button("Valider")
+        if submitted:
+            st.write("valeur 1", value_1, "valeur 2", value_2)
+
