64ca4f6f3c3093d379acfcb25a03948f598c000e
quentlavallee <jakimpact@gmail.com>
2024-01-22 22:32:39 +0100

    Réorganisation du dépot

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pickle
+import streamlit as st
 
 modalities = {
     "age": [18, 100],
@@ -25,126 +22,111 @@ bmi_scale = [
     {"category": "Obesity class III", "range": [40, float("inf")]},
     ]
 
-with open("modele.pkl", "rb") as file:
+data = pd.read_csv("project/csv/dataset_cleaned.csv")
+
+with open("project/modele.pkl", "rb") as file:
     model = pickle.load(file)
 file.close()
 
-st.title("Prédiction de la prime d'assurance d'Assur'Aimant")
-data = pd.read_csv("datasetClean.csv")
-
 form = {}
 
-st.sidebar.image("assuraimant.png", width=275)
-menu = st.sidebar.radio("Menu",["Accueil","Prédiction d'assurance"])
+st.title("Prédiction de primes d'assurance grâce à l'IA")
 
 
-if menu == "Accueil":
-    st.header("Statistiques des données")
-    if st.checkbox("Stats"):
-        st.table(data.describe())
-    if st.header("Graphique de corrélation"):
-        fig,ax = plt.subplots(figsize=(10,5))
-        sns.heatmap(data.corr(),annot=True,cmap="coolwarm")
-        st.pyplot(fig)
-    
-    st.title("Affichage des graphiques de données")
-    graph = st.selectbox("Types de graphiques :",["Diagramme de points","Diagramme de barres","Histogramme"])
-    if graph == "Diagramme de points":
-        value = st.slider("Filtrer les données selon l'age",18,65)
-        data = data.loc[data["age"]<=value]
-        fig,ax = plt.subplots(figsize=(10,5))
-        sns.scatterplot(data=data , x="bmi", y="charges",hue="smoker").set(title="Charges en fonction de l'IMC")
-        st.pyplot(fig)
-    if graph == "Diagramme de barres":
-        fig,ax = plt.subplots(figsize=(5,3))
-        sns.barplot(x="smoker",y="charges",data=data).set(title="Moyenne des charges pour les fumeurs et non fumeurs")
-        st.pyplot(fig)
-    if graph == "Histogramme":
-        fig,ax = plt.subplots(figsize=(5,3))
-        sns.histplot(data=data, x="bmi", kde=True, color="darkgreen").set(title="Répartition des individus selon l'IMC")
-        st.pyplot(fig)
+with st.form("Nouvel assuré"):
 
-if menu == "Prédiction d'assurance":
-    with st.form("Nouvel assuré"):
-        st.header("Estimation des charges du nouvel assuré:")
-        form["age"] = st.number_input(
-            "Age:",
-            min_value=modalities["age"][0],
-            max_value=modalities["age"][1],
-            value=None,
-            step=1,
-        )
-        form["sex"] = st.selectbox(
-            "Sexe:",
-            modalities["sex"],
-            index=None
-        )
-        form["bmi"] = st.number_input(
-            "IMC:",
-            min_value=modalities["bmi"][0],
-            max_value=modalities["bmi"][1],
-            value=None,
-            step=0.01,
-        )
+    st.header("Estimation des charges du nouvel assuré :")
 
-        st.write("Si vous ne connaissez pas votre IMC, veuillez remplir votre taille et votre poids.")
+    form["age"] = st.number_input(
+        "Age :",
+        min_value=modalities["age"][0],
+        max_value=modalities["age"][1],
+        value=None,
+        step=1,
+    )
 
-        height = st.number_input(
-            "Taille (en m):",
-            min_value=modalities["height"][0],
-            max_value=modalities["height"][1],
-            value=None,
-            step=0.01,
-        )
-        weight = st.number_input(
-            "Poids (en kg):",
-            min_value=modalities["weight"][0],
-            max_value=modalities["weight"][1],
-            value=None,
-            step=0.1,
-        )
-        form["children"] = st.number_input(
-            "Nombre d'enfants:",
-            min_value=modalities["children"][0],
-            max_value=modalities["children"][1],
-            value=None,
-            step=1
-        )
-        form["smoker"] = st.selectbox(
-            "Fumeur:",
-            modalities["smoker"],
-            index=None
-        )
-        form["region"] = st.selectbox(
-            "Region:",
-            modalities["region"],
-            index=None
-        )
-        submitted = st.form_submit_button("Valider")
-        if submitted:
-            for answer in form:
-                stop = False
-                if form[answer] == None:
-                    if answer == "bmi":
-                        if (height and weight) != None:
-                            form[answer] = weight / (height*2)
-                            continue
+    form["sex"] = st.selectbox(
+        "Genre :",
+        modalities["sex"],
+        index=None
+    )
 
-                    st.error(f"Missing answer for : {answer}")
-                    stop = True
-                if stop:
+    form["bmi"] = st.number_input(
+        "IMC :",
+        min_value=modalities["bmi"][0],
+        max_value=modalities["bmi"][1],
+        value=None,
+        step=0.01,
+    )
+
+    st.write("Si vous ne connaissez pas votre IMC, veuillez remplir votre taille et votre poids.")
+
+    height = st.number_input(
+        "Taille (en m) :",
+        min_value=modalities["height"][0],
+        max_value=modalities["height"][1],
+        value=None,
+        step=0.01,
+    )
+
+    weight = st.number_input(
+        "Poids (en kg) :",
+        min_value=modalities["weight"][0],
+        max_value=modalities["weight"][1],
+        value=None,
+        step=0.1,
+    )
+
+    form["children"] = st.number_input(
+        "Nombre d'enfants :",
+        min_value=modalities["children"][0],
+        max_value=modalities["children"][1],
+        value=None,
+        step=1
+    )
+
+    form["smoker"] = st.selectbox(
+        "Fumeur :",
+        modalities["smoker"],
+        index=None
+    )
+
+    form["region"] = st.selectbox(
+        "Region :",
+        modalities["region"],
+        index=None
+    )
+
+    submitted = st.form_submit_button("Valider")
+
+    if submitted:
+
+        for answer in form:
+            stop = False
+
+            if form[answer] == None:
+
+                if answer == "bmi":
+                    if (height and weight) != None:
+                        form[answer] = weight / (height*2)
+                        continue
+
+                st.error(f"Missing answer for : {answer}")
+                stop = True
+
+            if stop:
+                break
+
+        if not stop:
+
+            for group in bmi_scale:
+
+                if form["bmi"] >= group["range"][0] and form["bmi"] <= group["range"][1]:
+                    form["bmi"] = group["category"]
                     break
+                
+            df = pd.DataFrame(form, index=[0])
+            charges = model.predict(df)[0]
 
-            if not stop:
-
-                for group in bmi_scale:
-                    if form["bmi"] >= group["range"][0] and form["bmi"] <= group["range"][1]:
-                        form["bmi"] = group["category"]
-                        break
-                    
-                df = pd.DataFrame(form, index=[0])
-                charges = model.predict(df)[0]
-
-                st.divider()
-                st.write("Montant des charges : $", str(charges))
-
+            st.divider()
+            st.write("Montant des charges : $", str(charges))
